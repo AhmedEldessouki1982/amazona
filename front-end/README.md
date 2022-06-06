3f13b7102cca8daaac262e79dc6957a47d314ada
Ahmed Eldessouki <ahmed.dosoky1982@gmail.com>
2022-06-06 22:24:30 +0400

    first commit

---
+++ front-end/README.md
@@ -0,0 +1,1 @@
+Amazona: amazone clone stimulation
---
+++ front-end/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './navbar.scss';
+
+export default function Navbar () {
+    return (
+        <>
+            <div className='Navbar__header'>
+                <a href="/">Amazona</a> 
+            </div>
+                  
+        </>
+    )
+}
