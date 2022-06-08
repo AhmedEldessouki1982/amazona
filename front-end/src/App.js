2747b2f15faafbb5c4bc854638208f695bbe4a9a
Ahmed Eldessouki <ahmed.dosoky1982@gmail.com>
2022-06-08 22:55:46 +0400

    create product card component

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
-import Navbar from './components/Navbar';
+import Navbar from './components/navbar/Navbar'
+import Product from './components/productCard/Product';
 
 export default function App() {
   return (
     <>
       <Navbar />
+      <Product/>
     </>
   );
 }
